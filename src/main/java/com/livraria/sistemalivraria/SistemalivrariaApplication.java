81e149539a5a0218115b20acf181cef742a7fde7
E008215 <lucas.cavalcante@tst.jus.br>
2024-07-10 17:47:49 -0300

    first commit

---
+++ src/main/java/com/livraria/sistemalivraria/SistemalivrariaApplication.java
@@ -0,0 +1,13 @@
+package com.livraria.sistemalivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemalivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemalivrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livraria/sistemalivraria/SistemalivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livraria.sistemalivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemalivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
